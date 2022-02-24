d02ed7c8b87c6a9d79cb59d18b3415882ea4b4a7
Aravindh <83901906+aravindh-mb@users.noreply.github.com>
2022-02-25 00:26:47 +0530

    Add files via upload

---
+++ Notification API .js
@@ -0,0 +1,27 @@
+ function push_notification()
+    {
+              //check if browser supports notification API
+        if("Notification" in window)
+        {
+          //if the site already allow to show notification 
+            if(Notification.permission == "granted")
+            {
+                var notification = new Notification("Notification Title", {"body":"hello 🖐", "icon":"your image.jpeg"});
+            }
+            else
+            {
+              // it is default way to request show notification from user 
+                Notification.requestPermission(function (permission) {
+                    if (permission === "granted")
+                    {
+                        var notification = new Notification(" from Notification API", {"body":"hi🖐", "icon":"your image.jpeg"});
+                    }
+                });
+            }
+        }     
+        else
+        {
+            alert("Your browser doesn't support notfication API");
+        }      
+    }
+    push_notification();
